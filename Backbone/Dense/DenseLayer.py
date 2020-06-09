4e66661db1a3136365bdf23d0885e2ae17833bbc
HugPhat <50920961+HugPhat@users.noreply.github.com>
2020-06-10 00:02:24 +0700

    modified:   Backbone/Dense/DenseLayer.py 	modified:   Backbone/Dense/Tiramisu103.py

--- Backbone/Dense/DenseLayer.py
+++ Backbone/Dense/DenseLayer.py
@@ -1,6 +1,7 @@
-import torch 
-import torch.nn as nn 
-import torch.functional as F 
+import torch
+import torch.nn as nn
+import torch.functional as F
+
 
 class DenseLayer(nn.Module):
     def __init__(self, n_input, n_output):
@@ -13,13 +14,13 @@ class DenseLayer(nn.Module):
         module.add_module('ReLU', nn.ReLU(inplace=True))
         module.add_module('CONV', nn.Conv2d(
                           n_input, n_output,
-                          kernel_size=3, 
-                          padding= (3-1)//2,
+                          kernel_size=3,
+                          padding=(3-1)//2,
                           stride=1
                           ))
         module.add_module('Drop', nn.Dropout2d(0.2))
         return module
-        
+
     def forward(self, x):
         #if isinstance(x, torch.Tensor):
         #    inp = [x]
@@ -27,8 +28,9 @@ class DenseLayer(nn.Module):
         #    inp = x
         return self.module(x)
 
+
 class DenseBlock(nn.Module):
-    def __init__(self, n_input, n_output, n_layer, growth_rate, debug = False, for_upsample = False):
+    def __init__(self, n_input, n_output, n_layer, growth_rate, debug=False, for_upsample=False):
         super(DenseBlock, self).__init__()
         self.debug = debug
         self.for_upsample = for_upsample
@@ -39,50 +41,48 @@ class DenseBlock(nn.Module):
 
     def init_layer(self):
         n_input = self.n_input
-        n_out = self.growth_rate 
+        n_out = self.growth_rate
         for _ in range(1, self.n_layer + 1):
             tmp = DenseLayer(n_input, n_out)
             self.module.append(tmp)
             # cat : prev and curr
             n_input = n_input + self.growth_rate
-            n_out = self.growth_rate 
-            #if self.debug:
-            #    print(f'inp-> {n_input}')
-            #    print(tmp.module[2].weight.size())
-            #    print(f'out-> {n_out}')
+            n_out = self.growth_rate
 
     def forward(self, x):
         feature = x
         tmp = []
         for i, layer in enumerate(self.module):
             x = layer(feature)
-            if  self.for_upsample:
+            if self.for_upsample:
                 tmp.append(x)
             feature = torch.cat([feature, x], 1)
-        
-        if  self.for_upsample:    
-            return  feature#torch.cat(tmp, 1)
+
+        if self.for_upsample:
+            return feature, torch.cat(tmp, 1)
         else:
             return feature
-        
+
+
 class TransitionDown(nn.Module):
     def __init__(self, n_input, n_out):
-        super(TransitionDown,self).__init__()
+        super(TransitionDown, self).__init__()
         self.module = nn.Sequential()
         self.module.add_module('BN', nn.BatchNorm2d(n_input))
         self.module.add_module('ReLu', nn.ReLU(inplace=True))
         self.module.add_module('Conv', nn.Conv2d(
-                                in_channels=n_input,
-                                out_channels=n_out,
-                                kernel_size=(1,1),
-                                stride=1,
-                                padding= 0            
-                                ))
+            in_channels=n_input,
+            out_channels=n_out,
+            kernel_size=(1, 1),
+            stride=1,
+            padding=0
+        ))
         self.module.add_module('Drop', nn.Dropout2d(0.2))
-        self.module.add_module('MaxPool', nn.MaxPool2d(kernel_size=(2,2)))
-    
+        self.module.add_module('MaxPool', nn.MaxPool2d(kernel_size=(2, 2)))
+
     def forward(self, x):
         return self.module(x)
+
 
 '''
 class TransitionUp(nn.Module):
@@ -99,48 +99,50 @@ class TransitionUp(nn.Module):
         return x
 '''
 
+
 class TransitionUp(nn.Module):
     def __init__(self, n_inp, n_out):
         super(TransitionUp, self).__init__()
-        self.bn =  nn.BatchNorm2d(n_inp)
-        self.relu =  nn.ReLU(inplace=True)
-        self.conv =  nn.ConvTranspose2d(n_inp, n_out,
-                                          kernel_size=3, stride=2, 
-                                          padding=1,
-                                          bias=True,
-                                          )
+        self.bn = nn.BatchNorm2d(n_inp)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv = nn.ConvTranspose2d(n_inp, n_out,
+                                       kernel_size=3, stride=2,
+                                       padding=1,
+                                       bias=True,
+                                       )
         #self.add_module('pool', nn.AvgPool2d(kernel_size=2, stride=2))
+
     def forward(self, x, output_size):
         x = self.bn(x)
         x = self.relu(x)
         x = self.conv(x, output_size=output_size)
         return x
 
+
 class DownStep(nn.Module):
     def __init__(self, num, inp, out, growth_rate, ):
         super(DownStep, self).__init__()
-        self.dense_block = DenseBlock(inp, out, num, growth_rate, for_upsample=False)
+        self.dense_block = DenseBlock(
+            inp, out, num, growth_rate, for_upsample=False)
         self.down = TransitionDown(out, out)
 
     def forward(self, x):
         x1 = self.dense_block(x)
         x2 = self.down(x1)
         return x1, x2
-        
+
+
 class UpStep(nn.Module):
-    def __init__(self, num, inp_up, out_up, out, growth_rate):
+    def __init__(self, num, inp_up, inp, out, growth_rate):
         super(UpStep, self).__init__()
-        self.up = TransitionUp(inp_up, out_up)
-        self.dense_block = DenseBlock(inp_up, out, num, growth_rate, for_upsample=True)
-        
-    def forward(self, x, for_upsample, skip_connect):
-        
-        print(f'upping for_upsample => {for_upsample.size()}')
-        print(f'skip_connect => {skip_connect.size()}')
-        b,c,h,w = list(for_upsample.size())
-        for_upsample = self.up(for_upsample, output_size = [b,c,h*2,w*2])#, output_size = [b,c,h*2,w*2]
-        print(f'upped for_upsample => {for_upsample.size()}')
-        t = torch.cat([for_upsample, skip_connect], dim=1)
-        print(f'cat => {t.size()}')
-        forupsample= self.dense_block(t)
-        return forupsample
+        self.up = TransitionUp(inp_up, inp_up)
+        self.dense_block = DenseBlock(
+            inp, out, num, growth_rate, for_upsample=True)
+
+    def forward(self, x, skip_connect):
+        b, c, h, w = list(x.size())
+        # , output_size = [b,c,h*2,w*2]
+        x = self.up(x, output_size=[b, c, h*2, w*2])
+        t = torch.cat([x, skip_connect], dim=1)
+        x, forupsample = self.dense_block(t)
+        return x, forupsample
--- Backbone/Dense/Tiramisu103.py
+++ Backbone/Dense/Tiramisu103.py
@@ -5,19 +5,21 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 from DenseLayer import *
 
+
 class Tiramisu103(nn.Module):
-    def __init__(self, growth_rate = 16):
+    def __init__(self, growth_rate=16, flow=[4, 5, 7, 10, 12, 15], num_classes=10):
         super(Tiramisu103, self).__init__()
         self.growth_rate = growth_rate
         self.model = nn.ModuleList()
-        self.flow = [4, 5, 7, 10, 12]
+        self.flow = flow
         self.mid = 15
+        self.num_classes = num_classes
         self.Create()
 
     def FirstBlock(self):
-        x =  nn.Conv2d(in_channels= 3, out_channels= 48, 
-                        kernel_size=3, stride=1, 
-                        padding=(3-1)//2, bias=True)
+        x = nn.Conv2d(in_channels=3, out_channels=48,
+                      kernel_size=3, stride=1,
+                      padding=(3-1)//2, bias=True)
         return x
 
     def Create(self):
@@ -25,67 +27,54 @@ class Tiramisu103(nn.Module):
         inp = 48
         out = 48
         lout = []
-        # down 
-        for i in self.flow:
+        # down
+        for i in self.flow[:(len(self.flow)-1)]:
             out = inp + self.growth_rate*i
             lout.append(out)
-            print(out)
+            #print(out)
             self.model.append(DownStep(
-                                        num= i, 
-                                        inp= inp, 
-                                        out= out, 
-                                        growth_rate= self.growth_rate))
+                num=i,
+                inp=inp,
+                out=out,
+                growth_rate=self.growth_rate))
             inp = out
         # mid
         out = inp + self.growth_rate * self.mid
-        dense = DenseBlock(inp, out, self.mid, self.growth_rate, for_upsample= True)
-        #inp = out
+        dense = DenseBlock(inp, out, self.mid,
+                           self.growth_rate, for_upsample=True)
         self.model.append(dense)
         # up
         uppath = self.flow[::-1]
         lout = lout[::-1]
-        inp_up = out
-        for i, item in enumerate(uppath):
-            if item == max(uppath):
-                out_up = self.growth_rate*(self.mid)
-                out = out + self.growth_rate*item
-            else:
-                out_up = self.growth_rate*(uppath[i-1])
-                out = lout[i] + self.growth_rate*(uppath[i] + uppath[i-1]) 
-            
-            print(f'uu=>inp_up {inp_up}')
-            print(f'uu=>out_up {out_up}')
-            print(f'uu=>out {out}')
-            self.model.append(UpStep(
-                                    num=item,
-                                    inp_up=inp_up,
-                                    out_up=out_up,
-                                    #inp=inp,
-                                    out=out,
-                                    growth_rate=self.growth_rate))
-            inp_up = out
+        for i, item in enumerate(lout):
+          j = i+1
+          inp_up = self.growth_rate*(uppath[j-1])
+          #    #out = lout[i] + self.growth_rate*(uppath[i] + uppath[i-1])
+          inp = self.growth_rate*(uppath[j-1]) + lout[i]
+          out = self.growth_rate*(uppath[j])
+          self.model.append(UpStep(
+              num=uppath[j],
+              inp_up=inp_up,
+              #out_up=out_up,
+              inp=inp,
+              out=out,
+              growth_rate=self.growth_rate))
+        self.LastConv = nn.Conv2d(
+            in_channels=inp + out, out_channels=self.num_classes, kernel_size=1, stride=1, padding=0)
+        #self.Softmax = nn.Softmax2d
 
     def forward(self, x):
-        
         skip_connection = []
-
         x = self.model[0](x)
         i = 0
-        for it, step in enumerate(self.model[1: len(self.flow)+1]):
+        for it, step in enumerate(self.model[1: len(self.flow)]):
             i = it
             tmp, x = step(x)
-            print(f'i=>> {i}')
-            print(x.size())
             skip_connection.append(tmp)
-        for_upsample  = self.model[i+2](x)
-        #print(self.model[i+2])
-        print(for_upsample.size())
+        _, upsamp = self.model[i+2](x)
         neg_i = -1
         for it, step in enumerate(self.model[i+3:]):
-
-            for_upsample = step(x, for_upsample, skip_connection[neg_i])
+            x, upsamp = step(upsamp, skip_connection[neg_i])
             neg_i -= 1
-        
+        x = self.LastConv(x)
         return x
-            
-                
