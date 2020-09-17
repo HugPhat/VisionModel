4b7b4033a451b3cd2de0cd2ed7278e34a1c48ba6
hugphat <vohungphat99@gmail.com>
2020-09-17 19:34:14 +0700

    add hourglass model

--- Backbone/Hourglass/hourglass.py
+++ Backbone/Hourglass/hourglass.py
@@ -34,7 +34,7 @@ def basic_block(inp, use_bn = True):
         #print('dw')
         dws = dws_ratio*inp
         # Conv1x1
-        _basic_block.add_module('Con2d_1x1', nn.Conv2d(inp, dws, kernel_size=1, stride=1, padding=0))
+        _basic_block.add_module('Con2d_1x1', nn.Conv2d(inp, dws, kernel_size=1, stride=1, padding=0, groups=inp))
         if use_bn:
             _basic_block.add_module('BN_1x1', nn.BatchNorm2d(dws))
         _basic_block.add_module('Relu_1x1', nn.ReLU(inplace=True))
@@ -79,12 +79,13 @@ class down_stream(nn.Module):
 ############ Hourglass Module ##############
 
 class hourglass_module(nn.Module):
-    def __init__(self, input_size, basic_block_callback, n_reduce_blocks= 4,n_basis_blocks= 3, n_filter_blocks=3):
+    def __init__(self, input_size, basic_block_callback, n_reduce_blocks= 4,n_basis_blocks= 3, n_filter_blocks=3, use_bn= True):
         super(hourglass_module, self).__init__()
         self.inp = input_size
         self.n_reduce_blocks = n_reduce_blocks
         self.n_basis_blocks = n_basis_blocks
         self.n_filter_blocks = n_filter_blocks
+        self.use_bn= use_bn
         self.model = self.construct_module(basic_block_callback)
     
     def construct_module(self, basic_block_callback):
@@ -94,7 +95,7 @@ class hourglass_module(nn.Module):
         model = nn.ModuleDict({
                 'down' : nn.ModuleList(),
                 'up': nn.ModuleList(),
-                'post': basic_block_callback(inp, use_bn=True)
+                #'post': basic_block_callback(inp, use_bn=True)
             }
         )
         
@@ -103,16 +104,20 @@ class hourglass_module(nn.Module):
             basic_blocks = nn.Sequential()
             for j in range(self.n_basis_blocks):
                 basic_blocks.add_module('bblock_' + str(i) + '_' + str(j), 
-                                        basic_block_callback(inp, use_bn=True))
+                                        basic_block_callback(inp, use_bn=self.use_bn))
             basic_filter = nn.Sequential()
             for f in range(self.n_filter_blocks):
                 basic_filter.add_module('fblock_'+ str(i) + '_' + str(f),
-                                        basic_block_callback(inp, use_bn=True)
+                                        basic_block_callback(inp, use_bn=self.use_bn)
                                         )
                 
             basic_blocks = down_stream(basic_blocks, basic_filter, downSampleLayer((2,2), 2))
-            model['down'].append(basic_blocks)   
-            model['up'].append(nn.Upsample(scale_factor=2, mode='nearest'))
+            model['down'].append(basic_blocks) 
+              
+            up = nn.Sequential()
+            up.add_module('upsample_'+ str(i), nn.Upsample(scale_factor=2, mode='nearest'))
+            up.add_module('up_bblock'+ str(i), basic_block_callback(inp, use_bn= self.use_bn))
+            model['up'].append(up)
         
         
         return model
@@ -125,20 +130,98 @@ class hourglass_module(nn.Module):
             x1, x = block(x)
             branch.append(x1)
         # upsample 
-        for i, block in enumerate(self.model['up']):
+        for i in range(0, len(self.model['up'])):
             #print(x.size(), branch[-(i + 1)].size())
-            x = branch[-(i + 1)] + block(x)
-        
-        x = self.model['post'](x)
-        
+            x = branch[-(i + 1)] + self.model['up'][i](x)
+            
+        #print('x----', x.size())
         return x
         
+####### Intermediate Supervision ###########
+
+class intermediate_supervision(nn.Module):
+    def __init__(self, input_size,n_heatmaps, bb):
+        super(intermediate_supervision, self).__init__()
+        self.bb = bb
+        self.inter = nn.Conv2d(input_size, input_size, kernel_size=1, stride=1, padding=0)
+        self.output = nn.Sequential()
+        self.output.add_module('out_conv', 
+                               nn.Conv2d(input_size, n_heatmaps, kernel_size=1, stride=1, padding=0))
+        self.output.add_module('out_inter',
+                                nn.Conv2d(n_heatmaps, input_size, kernel_size=1, stride=1, padding=0)) 
+        
+    def forward(self, x):
+        y = self.bb(x)
+        y1 = self.inter(y)
+        y2 = self.output(y)
+        
+        y = y + y1 + y2
+        #print(y.size())
+        return y
         
 ###### Hourglass stacked ########
  
+def default_input_block(out):
+    prefix = 'default'
+    prefix_conv = lambda x: prefix+ '_conv2d_' + str(x)
+    prefix_relu = lambda x: prefix+ '_relu_' + str(x)
+    prefix_bn  = lambda x: prefix+ '_bn_' + str(x)
+    prefix_pool  = lambda x: prefix+ '_pool_' + str(x)
+    module = nn.Sequential()
+    module.add_module(prefix_conv(0), nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3))
+    module.add_module(prefix_bn(0), nn.BatchNorm2d(64))
+    module.add_module(prefix_relu(0), nn.ReLU(64))
+    module.add_module(prefix_conv('1x1_128'), nn.Conv2d(64, 128, kernel_size=1, stride=1, padding=0))
+    module.add_module(prefix_conv('bb0'), basic_block(128))
+    module.add_module(prefix_pool('maxpool'), nn.MaxPool2d(kernel_size=2, stride=2))
+    module.add_module(prefix_conv('1x1_256'), nn.Conv2d(128, out, kernel_size=1, stride=1, padding=0))
+    module.add_module(prefix_conv('bb1'), basic_block(out))
+    
+    return module
+    
+    
+    
+    
 class hourglass(nn.Module):
     
-    def __init__(self, stacked_ch=192, n_stacked_hg=3):
+    def __init__(self, 
+                 n_heatmaps,
+                 n_stacked_hg=3, 
+                 stacked_ch=192, 
+                 n_reduce_blocks=4, 
+                 n_basis_blocks=3, 
+                 n_filter_blocks=3,
+                 use_bn=True,
+                 input_block = None, 
+                 basic_block_callback=None,
+                 ):
         super(hourglass, self).__init__()
-        self.stacked_ch = stacked_ch 
         
+        self.n_heatmaps = n_heatmaps
+        #### 
+        self.stacked_ch = stacked_ch 
+        self.n_stacked_hg = n_stacked_hg
+        ####
+        self.n_reduce_blocks = n_reduce_blocks
+        self.n_basis_blocks    = n_basis_blocks
+        self.n_filter_blocks = n_filter_blocks
+        self.use_bn = use_bn
+        ####
+        self.bb_cb = basic_block_callback if basic_block_callback else basic_block
+        self.input_block = input_block if input_block else default_input_block
+        self.model = self.create_network()
+
+    def create_network(self):
+        model = nn.ModuleList()
+        model.append(self.input_block(self.stacked_ch))
+        for i in range(self.n_stacked_hg):
+            block = hourglass_module(self.stacked_ch, self.bb_cb, self.n_reduce_blocks, self.n_basis_blocks, self.n_filter_blocks, self.use_bn)
+            model.append(intermediate_supervision(self.stacked_ch, self.n_heatmaps, block))
+        return model
+    
+    def forward(self, x):
+        for i, block in enumerate(self.model):
+            #print(i)
+            x = block(x)
+            
+        return x
--- test/Backbone/Mobilenetv2/test_hourglass.py
+++ test/Backbone/Mobilenetv2/test_hourglass.py
@@ -9,32 +9,45 @@ from time import time
 
 channels = 192
 
-hg.use_bb_with_dw = False
-hg_module = hg.hourglass_module(input_size=channels, basic_block_callback= hg.basic_block)
-hg_module.eval()
+def test_module(channels):
 
-#print(hg_module)
+    hg.use_bb_with_dw = True
+    hg_module = hg.hourglass_module(input_size=channels, 
+                                    basic_block_callback= hg.basic_block)
+    hg_module.eval()
 
-x = torch.ones((1, channels, 64, 64))
+    #print(hg_module)
 
-## timing cpu
-for _ in range(5):
-    with torch.no_grad():
-        tic = time()
-        hg_module(x)
-        print(f'time: {round(time() - tic, 4)*1000} ms')
-## timing gpu
-hg_module.cuda()
-x = x.type(torch.cuda.FloatTensor).cuda()
-for _ in range(5):
-    with torch.no_grad():
-        tic = time()
-        hg_module(x)
-        print(f'time: {round(time() - tic, 4)*1000} ms')
+    x = torch.ones((1, channels, 64, 64))
 
-hg_module.cpu()
-# number of paramters
-s = 0
-for m in hg_module.parameters():
+    ## timing cpu
+    for _ in range(5):
+        with torch.no_grad():
+            tic = time()
+            hg_module(x)
+            print(f'time: {round(time() - tic, 4)*1000} ms')
+    ## timing gpu
+    hg_module.cuda()
+    x = x.type(torch.cuda.FloatTensor).cuda()
+    for _ in range(5):
+        with torch.no_grad():
+            tic = time()
+            hg_module(x)
+            print(f'time: {round(time() - tic, 4)*1000} ms')
+
+    hg_module.cpu()
+    # number of paramters
+    s = 0
+    for m in hg_module.parameters():
+        s += m.cpu().numel()
+    print(s)
+
+#####
+
+x = torch.ones((1, 3, 256, 256))
+hg_model = hg.hourglass(19)
+hg_model(x)
+s =0 
+for m in hg_model.parameters():
     s += m.cpu().numel()
 print(s)
