cca9c3992634741df390bc531c77a86b605741e6
hugphat <50920961+HugPhat@users.noreply.github.com>
2020-12-09 10:37:45 +0700

    add head

--- Head/Yolov3/Utils.py
+++ Head/Yolov3/Utils.py
@@ -5,6 +5,7 @@ import torch.nn.functional as F
 import math
 import numpy as np
 
+
 def bbox_iou(box1, box2, x1y1x2y2=True):
     """
     Returns the IoU of two bounding boxes
@@ -17,14 +18,16 @@ def bbox_iou(box1, box2, x1y1x2y2=True):
         b2_y1, b2_y2 = box2[:, 1] - box2[:, 3] / 2, box2[:, 1] + box2[:, 3] / 2
     else:
         # Get the coordinates of bounding boxes
-        b1_x1, b1_y1, b1_x2, b1_y2 = box1[:, 0], box1[:, 1], box1[:, 2], box1[:, 3]
-        b2_x1, b2_y1, b2_x2, b2_y2 = box2[:, 0], box2[:, 1], box2[:, 2], box2[:, 3]
+        b1_x1, b1_y1, b1_x2, b1_y2 = box1[:,
+                                          0], box1[:, 1], box1[:, 2], box1[:, 3]
+        b2_x1, b2_y1, b2_x2, b2_y2 = box2[:,
+                                          0], box2[:, 1], box2[:, 2], box2[:, 3]
 
     # get the corrdinates of the intersection rectangle
     inter_rect_x1 = torch.max(b1_x1, b2_x1)
     inter_rect_y1 = torch.max(b1_y1, b2_y1)
-    inter_rect_x2 = torch.min(b1_x2, b2_x2)
-    inter_rect_y2 = torch.min(b1_y2, b2_y2)
+    inter_rect_x2 = torch.min(b1_x2, b2_x2).clamp(min=0)
+    inter_rect_y2 = torch.min(b1_y2, b2_y2).clamp(min=0)
     # Intersection area
     inter_area = torch.clamp(inter_rect_x2 - inter_rect_x1 + 1, min=0) * torch.clamp(
         inter_rect_y2 - inter_rect_y1 + 1, min=0
@@ -37,93 +40,89 @@ def bbox_iou(box1, box2, x1y1x2y2=True):
 
     return iou
 
-def TargetBuilder(PredBoxes,
-                  Conf,
-                  Class,
-                  targets,
-                  anchors,
-                  n_class,
-                  n_grid1,
-                  n_grid2,
-                  ignore_thresh = 0.5,
-                  iou_thresh = 0.5,
-                  class_thresh = 0.5,
-                  ):
-    NB = PredBoxes.size(0) # num  batches
-    
-    NA = len(anchors) # num anchors
-    NG1 = n_grid1
-    NG2 = n_grid2
-    
-    cell_len = 5 + n_class
-    
-    #mask of objectness score
-    obj_mask = torch.zeros(NB, NA, NG1, NG2)
-    # For loss calculation
-    tx = torch.zeros(NB, NA, NG1, NG2)
-    ty = torch.zeros(NB, NA, NG1, NG2)
-    tw = torch.zeros(NB, NA, NG1, NG2)
-    th = torch.zeros(NB, NA, NG1, NG2)
-    
-    tconf  = torch.zeros(NB, NA, NG1, NG2)
-    tclass = torch.zeros(NB, NA, NG1, NG2, n_class)
-    
-    N_GroundTruth = 0
-    N_Correct = 0
-    
-    # Go through every batches
-    for batch in range(NB):
-        # at this iteration , go through this target
-        for targ in range(targets.size(1)):
-            N_GroundTruth +=1
-            # find the grid cell of (x, y)
-            ngx = targets[batch, targ, 1] * NG1
-            ngy = targets[batch, targ, 2] * NG2
-            ngw = targets[batch, targ, 3] * NG1
-            ngh = targets[batch, targ, 4] * NG2
-        
-            ngi = int(ngx) if ngx < NG1 else NG1-1
-            ngj = int(ngy) if ngy < NG2 else NG2-1
-  
-            gt_box = torch.FloatTensor(np.array([0, 0, ngw, ngh])).unsqueeze(0)
+
+
+def build_targets(
+    pred_boxes,
+    pred_conf,
+    pred_cls,
+    target,
+    anchors,
+    num_anchors,
+    num_classes,
+    grid_size,
+    ignore_thres,
+    IoU_thresh=0.5,
+    score_thresh=0.5
+):
+    BoolTensor = torch.cuda.BoolTensor if pred_boxes.is_cuda else torch.BoolTensor
+    FloatTensor = torch.cuda.FloatTensor if pred_boxes.is_cuda else torch.FloatTensor
+    LongTensor = torch.cuda.LongTensor if pred_boxes.is_cuda else torch.LongTensor
+
+    nB = target.size(0)
+    nA = num_anchors
+    nC = num_classes
+    nG = grid_size
+    mask = BoolTensor(nB, nA, nG, nG).fill_(0)
+    noobj_mask = BoolTensor(nB, nA, nG, nG).fill_(1)
+    tx = FloatTensor(nB, nA, nG, nG).fill_(0)
+    ty = FloatTensor(nB, nA, nG, nG).fill_(0)
+    tw = FloatTensor(nB, nA, nG, nG).fill_(0)
+    th = FloatTensor(nB, nA, nG, nG).fill_(0)
+    tconf = BoolTensor(nB, nA, nG, nG).fill_(0)
+    tcls = BoolTensor(nB, nA, nG, nG, nC).fill_(0)
+
+    nGT = 0
+    nCorrect = 0
+    for b in range(nB):
+        for t in range(target.shape[1]):
+            if target[b, t].sum() == 0:
+                continue
+            nGT += 1
+            # Convert to position relative to box
+            gx = target[b, t, 1] * nG
+            gy = target[b, t, 2] * nG
+            gw = target[b, t, 3] * nG
+            gh = target[b, t, 4] * nG
+            # Get grid box indices
+            gi = gx.floor().type(LongTensor)
+            gj = gy.floor().type(LongTensor)
+            # Get shape of gt box
+            gt_box = FloatTensor([0, 0, gw, gh]).unsqueeze(0)
             # Get shape of anchor box
-            anchor_shapes = torch.FloatTensor(np.concatenate(
-                (np.zeros((len(anchors), 2)), np.array(anchors)), 1))
-            
+            anchor_shapes = FloatTensor(torch.cat(
+                (FloatTensor(len(anchors), 2).fill_(0).data, FloatTensor(anchors).data), 1))
             # Calculate iou between gt and anchor shapes
             anch_ious = bbox_iou(gt_box, anchor_shapes)
-            # Where the overlap is larger than threshold set mask to zero (ignore)
-            obj_mask[batch, anch_ious > ignore_thresh, ngj, ngi] = 1
-            
-            # best fit anchor
-            best_anchor = np.argmax(anch_ious)
-            mask[batch,  ngi, ngj]
-            
-            # Ground truth box
-            gt_box = torch.FloatTensor(np.array([ngx, ngy, ngw, ngh])).unsqueeze(0)
-            # Best predicted box
-            pred_box = PredBoxes[batch, best_anchor, ngi, ngj]
-            # 
-            tx[batch, best_anchor, ngi, ngj] = ngx -ngi
-            ty[batch, best_anchor, ngi, ngj] = ngy -ngj
-            
-            tw[batch, best_anchor, ngi, ngj] = math.log( ngw /(anchors[best_anchor][0] + 1e-16))
-            th[batch, best_anchor, ngi, ngj] = math.log( ngh /(anchors[best_anchor][1] + 1e-16))
-            
-            tar_label = int(targets[batch, targ, 0])
-            
-            tclass[batch, best_anchor, ngi, ngj, tar_label] = 1
-            tconf[batch, best_anchor, ngi, ngj] = 1
-            
-            iou = bbox_iou(gt_box, pred_box,x1y1x2y2=False)
-            
-            pred_label = torch.argmax(Class[batch, best_anchor, ngi, ngj, :])
-            score = Conf[batch, best_anchor, ngi, ngj]
-            
-            if score > class_thresh and \
-                pred_label == targets and \
-                    iou > iou_thresh:
-                N_Correct += 1
-    
-    return tx, ty, tw, th, tclass, tconf, obj_mask, N_GroundTruth, N_Correct
-                        
+            # Where the overlap is larger than ignoring threshold set mask to zero (ignore)
+            #
+            noobj_mask[b, anch_ious > ignore_thres, gj, gi] = 0  # $warn
+            #
+            # Find the best matching anchor box
+            best_n = torch.argmax(anch_ious)
+            # Get ground truth box
+            gt_box = FloatTensor([gx, gy, gw, gh]).unsqueeze(0)
+            # Get the best prediction
+            pred_box = pred_boxes[b, best_n, gj, gi].unsqueeze(0)
+            # Masks
+            mask[b, best_n, gj, gi] = 1
+            noobj_mask[b, best_n, gj, gi] = 0
+            # Coordinates
+            tx[b, best_n, gj, gi] = (gx - gi).clamp(EPSILON, 1 - EPSILON)
+            ty[b, best_n, gj, gi] = (gy - gj).clamp(EPSILON, 1 - EPSILON)
+            # Width and height
+            tw[b, best_n, gj, gi] = torch.log(
+                gw / (anchors[best_n][0] + 1e-8)).clamp(min=EPSILON)
+            th[b, best_n, gj, gi] = torch.log(
+                gh / (anchors[best_n][1] + 1e-8)).clamp(min=EPSILON)
+            # One-hot encoding of label
+            target_label = int(target[b, t, 0].cpu().data)
+            tcls[b, best_n, gj, gi, target_label] = 1
+            tconf[b, best_n, gj, gi] = 1
+
+            iou = bbox_iou(gt_box, pred_box, x1y1x2y2=False)
+            pred_label = torch.argmax(pred_cls[b, best_n, gj, gi]).cpu().data
+            score = pred_conf[b, best_n, gj, gi]
+            if iou > IoU_thresh and pred_label == target_label and score.item() > score_thresh:
+                nCorrect += 1
+    return nGT, nCorrect, mask, noobj_mask, tx, ty, tw, th, tconf, tcls
--- Head/Yolov3/yolov3_head.py
+++ Head/Yolov3/yolov3_head.py
@@ -1,121 +1,201 @@
-import torch 
+from torch.autograd import Variable
 import torch.nn as nn
-import torch.nn.functional as F 
+import torch
+from Yolov3.Utils.utils import build_targets
 
-import numpy as np 
 
-class YoloPreHead(nn.Module):
-    def __init__(self,
-                 batch,
-                 CH,
-                 Grid1,
-                 Grid2,
-                 n_anchor,
-                 n_class
+class yoloHeadv3(nn.Module):
+    def __init__(self, anchor,
+                 num_classes,
+                 img_size,
+                 lb_noobj=1.0,
+                 lb_obj=1.0,
+                 lb_class=1.0,
+                 lb_pos=1.0,
+                 ignore_thresh=0.5,
                  ):
-        super(YoloPreHead, self).__init__()
-        '''
-        The last layer before YoloHead
-        k_size: 1x1
-        + Input: [Batch, CH,Grid1, Grid2]
-        + Output: [Batch, [n_anchor *(4 + 1 + num_class)], Grid1, Grid2]
-        '''
-        self.conv1x1 = nn.Conv2d(
-                                in_channels=CH,
-                                out_channels= (n_anchor * (4 + 1 + n_class)),
-                                kernel_size=(1,1),
-                                padding=0,
-                                stride=1
-                                )
-        
-        
-    def forward(self, x):
-        return self.conv1x1(x)
+        """Yolo detection layer
 
-class YoloHead(nn.Module):
-    def __init__(self, 
-                    anchor,
-                    num_classes,
-                    img_size,
-                    is_train, 
-                    ignore_thresh,
-                    lambda_noobj,
-                    lambda_obj,
-                    lambda_box
-                    ):
-        super(YoloHead, self).__init__()
-        '''
-        Head (output) of Yolov3
-        + Input: 
-        Tensor from YoloPreHead
-            => Applying author formular:
-                + bx = sig(tx) + cx
-                + by = sig(ty) + cy
-                + bw = pw * exp(tw)
-                + bh = ph * exp(th)
-                + conf = sig(conf)
-                + class = sig(class)
-        + Output: 
-            Tensor [n_batch, grid1*grid2*n_anchor, 5+ n_class]
-        '''
+        Args:
+            anchor ([list]): [list of anchor box]
+            num_classes ([int]): [number of training classes]
+            img_size ([int]): [fixed size image]
+            lb_noobj (float, optional): [No function at this moment]. Defaults to 1.0.
+            lb_obj (float, optional): [No function at this moment]. Defaults to 1.0.
+            lb_class (float, optional): [No function at this moment]. Defaults to 1.0.
+            lb_pos (float, optional): [No function at this moment]. Defaults to 1.0.
+            ignore_thresh (float, optional): [threshold of objecness score]. Defaults to 0.5.
+        """
+        super(yoloHeadv3, self).__init__()
         self.anchor = anchor
-        self.n_anchor = len(anchor)
+        self.num_anchor = len(self.anchor)
         self.num_classes = num_classes
-        self.cell_len = 5 + num_classes
         self.img_size = img_size
-        self.ignore_thresh = ignore_thresh
-        self.lambda_noobj = lambda_noobj
-        self.lambda_obj = lambda_obj
-        self.lambda_box = lambda_box
-        
-    def forward(self, x):
-        
-        n_batch = x.size(0)
-        n_grid1 = x.size(2)
-        n_grid2 = x.size(3)
-        
-        # [n_batch, n_anchor*( len_cell), grid1, grid2]
-        # -> [n_batch, n_anchor, grid1, grid2, len_cell]
-        x = x.view(n_batch, self.n_anchor, n_grid1, n_grid2, self.cell_len).contiguous()
-        
-        grid1_ratio = img_size[0]/ n_grid1
-        grid2_ratio = img_size[1]/ n_grid2
-        
-        FT = torch.cuda.FloatTensor if x.is_cuda() else torch.FloatTensor
-        LT = torch.cuda.LongTensor if x.is_cuda() else torch.LongTensor
-        BT = torch.cuda.BoolTensor if x.is_cuda() else torch.BoolTensor
-        
-        ########## Box Coordinate #############
-        x = torch.sigmoid(x[..., 0])
-        y = torch.sigmoid(x[..., 1])
-        w = x[..., 2]
-        h = x[..., 3]
-        coor_x = torch.arange(n_grid1).repeat(n_grid1, 1).view(
-                [1, 1, n_grid1, n_grid2]).type(FT)
-        coor_y = torch.arange(n_grid2).repeat(n_grid2, 1).t().view(
-                [1, 1, n_grid1, n_grid2]).type(FT)
-        scaled_anchors = FT([(aw/ grid1_ratio, ah/ grid2_ratio) for aw, ah in self.anchor])
-        anchor_x = scaled_anchors[:, :1].view((1, self.n_anchor, 1, 1))
-        anchor_y = scaled_anchors[:, 1:].view((1, self.n_anchor, 1, 1))
-        ########### Conf / Class score #########
-        Conf = torch.sigmoid(x[..., 4])
-        Class = torch.sigmoid(x[..., 5:])
-        ########################################
-        PredBoxes = FT(x[..., :4].size()) # [n_batch, n_anchor, grid1, grid2, 4]
-        # bx, by
-        PredBoxes[..., 0] = x.data + coor_x
-        PredBoxes[..., 1] = y.data + coor_y
-        # w, h
-        PredBoxes[..., 2] = torch.exp(w)*anchor_x
-        PredBoxes[..., 3] = torch.exp(h)*anchor_y
-        
-        output = torch.cat(
-            [
-                PredBoxes.view(n_batch, self.n_anchor,  n_grid1, n_grid2, 4),
-                Conf.view(n_batch, self.n_anchor,  n_grid1, n_grid2, 1),
-                Class.view(n_batch, self.n_anchor,  n_grid1, n_grid2, self.num_classes)
-            ], dim=-1
-        )
-        
-        return output
-    
+        self.grid_info = num_classes + 5
+
+        self.ignore_thres = ignore_thresh
+        self.lb_noobj = lb_noobj
+        self.lb_obj = lb_obj
+        self.lb_class = lb_class
+        self.lb_pos = lb_pos
+
+        self.mse_loss = nn.MSELoss()
+        self.bce_loss = nn.BCELoss()
+        self.ce_loss = nn.CrossEntropyLoss()
+
+        self.confidence_points = [0.5, 0.75, 0.85]
+
+    def calculate_precision(self, nCorrect, _tensor) -> dict:
+        """
+        Returns:
+            * {rangeA : value , rangeB: value, ...}
+        """
+        precision = {}
+        for each in self.confidence_points:
+            nProposals = float((_tensor.cpu() > each).sum())
+            if nProposals > 0:
+                precision.update({'P'+str(each): float(nCorrect / nProposals)})
+            else:
+                precision.update({'P'+str(each): 0})
+
+        return precision
+
+    def forward(self, X, targets=None):
+        """[Feed forward function]
+
+        Args:
+            X ([tensor]): [input tensor shape (batch_size, 3, img_size, img_size)]
+            targets ([tensor], optional): [tensor shape (batch_size, max_objects, 5 + number of classes)]. Defaults to None.
+
+        Returns:
+            [{
+                "mask"      : mask,
+                "x"         : [x, tx],
+                "y"         : [y, ty],
+                "w"         : [w, tw],
+                "h"         : [h, th],
+                "conf"      : [conf, tconf],
+                "class"     : [clss, tcls],
+                "recall"    : recall,
+                "precision" : precision
+            }]: [Pairs of value to calculate loss function]
+        """
+
+        isTrain = True if targets != None else False
+        numBatches = X.size(0)
+        numGrids = X.size(2)
+
+        ratio = self.img_size / numGrids
+
+        FloatTensor = torch.cuda.FloatTensor if X.is_cuda else torch.FloatTensor
+        LongTensor = torch.cuda.LongTensor if X.is_cuda else torch.LongTensor
+        BoolTensor = torch.cuda.BoolTensor if X.is_cuda else torch.BoolTensor
+
+        predict = X.view(numBatches, self.num_anchor,
+                         self.grid_info, numGrids, numGrids).permute(0, 1, 3, 4, 2).contiguous()  #
+
+        x = torch.sigmoid(predict[..., 0])
+        y = torch.sigmoid(predict[..., 1])
+        w = predict[..., 2]
+        h = predict[..., 3]
+
+        conf = torch.sigmoid(predict[...,   4])
+        clss = torch.sigmoid(predict[...,   5:])
+        #print(clss)
+        coor_x = torch.arange(numGrids).repeat(numGrids, 1).view(
+            [1, 1, numGrids, numGrids]).type(FloatTensor)
+        coor_y = torch.arange(numGrids).repeat(numGrids, 1).t().view(
+            [1, 1, numGrids, numGrids]).type(FloatTensor)
+        scale_anchors = FloatTensor([(aw/ratio, ah/ratio)
+                                     for aw, ah in self.anchor])
+        anchor_x = scale_anchors[:, 0:1].view((1, self.num_anchor, 1, 1))
+        anchor_y = scale_anchors[:, 1:2].view((1, self.num_anchor, 1, 1))
+
+        pred_boxes = FloatTensor(predict[..., :4].shape)
+        pred_boxes[..., 0] = x + coor_x
+
+        pred_boxes[..., 1] = y + coor_y
+        pred_boxes[..., 2] = torch.exp(w).clamp(max=1E3) * anchor_x
+        pred_boxes[..., 3] = torch.exp(h).clamp(max=1E3) * anchor_y
+
+        if torch.isnan(conf).any():
+            print(f'predict { predict } size {predict.size()}')
+            print(clss)
+            print(predict[..., 4])
+        if isTrain:
+            #if x.is_cuda:
+            #    self.mse_loss.cuda()
+            #    self.ce_loss.cuda()
+            #    self.bce_loss.cuda()
+
+            nGT, nCorrect, mask, noobj_mask, tx, ty, tw, th, tconf, tcls = build_targets(
+                pred_boxes=pred_boxes,
+                pred_conf=conf,
+                pred_cls=clss,
+                target=targets,
+                anchors=scale_anchors,
+                num_anchors=self.num_anchor,
+                num_classes=self.num_classes,
+                grid_size=numGrids,
+                ignore_thres=self.ignore_thres,
+
+            )
+
+            recall = float(nCorrect / nGT) if nGT else 1
+            precision = self.calculate_precision(nCorrect, conf)
+
+            # Handle masks
+            mask = Variable(mask.type(BoolTensor))
+            noobj_mask = Variable(noobj_mask.type(BoolTensor))
+
+            # Handle target variables
+            tx = Variable(tx.type(FloatTensor), requires_grad=False)
+            ty = Variable(ty.type(FloatTensor), requires_grad=False)
+            tw = Variable(tw.type(FloatTensor), requires_grad=False)
+            th = Variable(th.type(FloatTensor), requires_grad=False)
+            tconf = Variable(tconf.type(FloatTensor), requires_grad=False)
+            tcls = Variable(tcls.type(LongTensor), requires_grad=False)
+
+            # Mask outputs to ignore non-existing objects
+            loss_x = self.mse_loss(x[mask], tx[mask])
+            loss_y = self.mse_loss(y[mask], ty[mask])
+            loss_w = self.mse_loss(w[mask], tw[mask])
+            loss_h = self.mse_loss(h[mask], th[mask])
+            loss_conf = self.bce_loss(conf[mask], tconf[mask])*self.lb_obj +\
+                self.bce_loss(conf[noobj_mask],
+                              tconf[noobj_mask])*self.lb_noobj
+
+            loss_cls = self.ce_loss(clss[mask], torch.argmax(tcls[mask], 1))
+            loss_pos = (loss_x + loss_y + loss_w + loss_h)
+            loss = loss_pos * self.lb_pos + loss_conf + loss_cls*self.lb_class
+
+            if torch.sum(mask) == 0 or torch.isnan(loss_conf) or torch.isinf(loss_conf):
+                print(
+                    f'Target tensor max {torch.max(targets)} | min {torch.min(targets)}')
+
+            return (
+                loss,
+                {
+                    "total": loss.item(),
+                    "loss_pos": loss_pos.item(),
+                    "loss_x": loss_x.item(),
+                    "loss_y": loss_y.item(),
+                    "loss_w": loss_w.item(),
+                    "loss_h": loss_h.item(),
+                    "loss_conf": loss_conf.item(),
+                    "loss_class": loss_cls.item(),
+                    "recall": recall,
+                    **precision
+                }
+            )
+
+        else:
+            output = torch.cat(
+                (
+                    pred_boxes.view(numBatches, -1, 4)*ratio,
+                    conf.view(numBatches, -1, 1),
+                    clss.view(numBatches, -1, self.num_classes),
+                ), -1,
+            )
+
+            return output
